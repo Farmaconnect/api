eab73db8ea192222eb7695d83971041dc9e278ea
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2021-02-02 13:14:00 +0100

    Ejemplo Object C

---
+++ examples_token/objc.c
@@ -0,0 +1,59 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    NSString* path = @"https://rest.farmaconnect.es/";
+    NSArray* array = @[
+                         // Request parameters
+                         @"entities=true",
+                         @"includes={string}",
+                         @"excludes={string}",
+                      ];
+    
+    NSString* string = [array componentsJoinedByString:@"&"];
+    path = [path stringByAppendingFormat:@"?%@", string];
+
+    NSLog(@"%@", path);
+
+    NSMutableURLRequest* _request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:path]];
+    [_request setHTTPMethod:@"GET"];
+    // Request headers
+    [_request setValue:@"{subscription key}" forHTTPHeaderField:@"TOKEN_KEY"];
+    // Request body
+    [_request setHTTPBody:[@"{body}" dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSURLResponse *response = nil;
+    NSError *error = nil;
+    NSData* _connectionData = [NSURLConnection sendSynchronousRequest:_request returningResponse:&response error:&error];
+
+    if (nil != error)
+    {
+        NSLog(@"Error: %@", error);
+    }
+    else
+    {
+        NSError* error = nil;
+        NSMutableDictionary* json = nil;
+        NSString* dataString = [[NSString alloc] initWithData:_connectionData encoding:NSUTF8StringEncoding];
+        NSLog(@"%@", dataString);
+        
+        if (nil != _connectionData)
+        {
+            json = [NSJSONSerialization JSONObjectWithData:_connectionData options:NSJSONReadingMutableContainers error:&error];
+        }
+        
+        if (error || !json)
+        {
+            NSLog(@"Could not parse loaded json with error:%@", error);
+        }
+        
+        NSLog(@"%@", json);
+        _connectionData = nil;
+    }
+    
+    [pool drain];
+
+    return 0;
+}
